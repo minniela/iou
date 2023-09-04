fd31c3585d54b61315b99d63a993a02a681a67e7
lukebuthman <lbuthman@gmail.com>
2023-09-04 12:53:04 -0500

    implement first step with basic layout

--- iou-app/initial-code-scan/ui-components/lwc/iouSetupInstructions/iouSetupInstructions.js
+++ iou-app/initial-code-scan/ui-components/lwc/iouSetupInstructions/iouSetupInstructions.js
@@ -2,4 +2,9 @@ import {LightningElement} from 'lwc';
 
 export default class IouSetupInstructions extends LightningElement {
 
+    nextStepInstruction = 'Install SFCA:';
+
+    handleNextStep() {
+        this.nextStepInstruction = 'Run SFCA:';
+    }
 }
