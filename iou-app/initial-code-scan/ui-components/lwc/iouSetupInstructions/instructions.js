4bd6d3a574ac8d7cc9224da9492d6a46cfa4f1dd
lukebuthman <lbuthman@gmail.com>
2023-09-04 13:26:10 -0500

    move instruction into their own file for better soc

---
+++ iou-app/initial-code-scan/ui-components/lwc/iouSetupInstructions/instructions.js
@@ -0,0 +1,41 @@
+const getInstructions = () => {
+    return [
+        {
+            title: 'Download Ruleset:',
+            instruction: '',
+            link: ''
+        },
+        {
+            title: 'Install SFCA:',
+            instruction: 'sfdx plugins:install @salesforce/sfdx-scanner',
+            link: ''
+        },
+        {
+            title: 'Run SFCA:',
+            instruction: 'sfdx scanner:run --format json --target "force-app" --outfile "scans/initialCodeScan.json" --pmdconfig "scans/level-0-tech-debt-ruleset.xml"',
+            link: ''
+        },
+        {
+            title: 'Upload JSON:',
+            instruction: '',
+            link: ''
+        },
+        {
+            title: 'Initialize App:',
+            instruction: '',
+            link: ''
+        },
+        {
+            title: 'Audit Triggers:',
+            instruction: '',
+            link: ''
+        },
+        {
+            title: 'All Set:',
+            instruction: '',
+            link: ''
+        }
+    ];
+}
+
+export {getInstructions};
--- iou-app/initial-code-scan/ui-components/lwc/iouSetupInstructions/iouSetupInstructions.js
+++ iou-app/initial-code-scan/ui-components/lwc/iouSetupInstructions/iouSetupInstructions.js
@@ -1,50 +1,13 @@
 import {LightningElement} from 'lwc';
+import {getInstructions} from './instructions';
 
 export default class IouSetupInstructions extends LightningElement {
 
     nextStepIndex = 0
-    nextStepInstruction = STEPS[this.nextStepIndex].title;
+    nextStepInstruction = getInstructions()[this.nextStepIndex].title;
 
     handleNextStep() {
         this.nextStepIndex += 1;
-        this.nextStepInstruction = STEPS[this.nextStepIndex].title;
+        this.nextStepInstruction = getInstructions()[this.nextStepIndex].title;
     }
 }
-
-const STEPS = [
-    {
-        title: 'Download Ruleset:',
-        instruction: '',
-        link: ''
-    },
-    {
-        title: 'Install SFCA:',
-        instruction: 'sfdx plugins:install @salesforce/sfdx-scanner',
-        link: ''
-    },
-    {
-        title: 'Run SFCA:',
-        instruction: 'sfdx scanner:run --format json --target "force-app" --outfile "scans/initialCodeScan.json" --pmdconfig "scans/level-0-tech-debt-ruleset.xml"',
-        link: ''
-    },
-    {
-        title: 'Upload JSON:',
-        instruction: '',
-        link: ''
-    },
-    {
-        title: 'Initialize App:',
-        instruction: '',
-        link: ''
-    },
-    {
-        title: 'Audit Triggers:',
-        instruction: '',
-        link: ''
-    },
-    {
-        title: 'All Set:',
-        instruction: '',
-        link: ''
-    }
-]
