4f81027ced096aafe008ffd6c0497231f209e0d5
lukebuthman <lbuthman@gmail.com>
2023-09-04 13:15:27 -0500

    create instruction JSON and navigate on button click

--- iou-app/initial-code-scan/ui-components/lwc/iouSetupInstructions/iouSetupInstructions.js
+++ iou-app/initial-code-scan/ui-components/lwc/iouSetupInstructions/iouSetupInstructions.js
@@ -2,9 +2,49 @@ import {LightningElement} from 'lwc';
 
 export default class IouSetupInstructions extends LightningElement {
 
-    nextStepInstruction = 'Install SFCA:';
+    nextStepIndex = 0
+    nextStepInstruction = STEPS[this.nextStepIndex].title;
 
     handleNextStep() {
-        this.nextStepInstruction = 'Run SFCA:';
+        this.nextStepIndex += 1;
+        this.nextStepInstruction = STEPS[this.nextStepIndex].title;
     }
 }
+
+const STEPS = [
+    {
+        title: 'Download Ruleset:',
+        instruction: '',
+        link: ''
+    },
+    {
+        title: 'Install SFCA:',
+        instruction: 'sfdx plugins:install @salesforce/sfdx-scanner',
+        link: ''
+    },
+    {
+        title: 'Run SFCA:',
+        instruction: 'sfdx scanner:run --format json --target "force-app" --outfile "scans/initialCodeScan.json" --pmdconfig "scans/level-0-tech-debt-ruleset.xml"',
+        link: ''
+    },
+    {
+        title: 'Upload JSON:',
+        instruction: '',
+        link: ''
+    },
+    {
+        title: 'Initialize App:',
+        instruction: '',
+        link: ''
+    },
+    {
+        title: 'Audit Triggers:',
+        instruction: '',
+        link: ''
+    },
+    {
+        title: 'All Set:',
+        instruction: '',
+        link: ''
+    }
+]
