07ea92b7f383f8e1559f9351e6e78b0a677bca5b
lukebuthman <lbuthman@gmail.com>
2023-09-04 18:31:41 -0500

    rename for precision

--- iou-app/initial-code-scan/ui-components/lwc/iouSetupInstructions/iouSetupInstructions.js
+++ iou-app/initial-code-scan/ui-components/lwc/iouSetupInstructions/iouSetupInstructions.js
@@ -4,10 +4,10 @@ import {getInstructions} from './instructions';
 export default class IouSetupInstructions extends LightningElement {
 
     nextStepIndex = 0
-    nextStepInstruction = getInstructions()[this.nextStepIndex].title;
+    nextStepTitle = getInstructions()[this.nextStepIndex].title;
 
     handleNextStep() {
         this.nextStepIndex += 1;
-        this.nextStepInstruction = getInstructions()[this.nextStepIndex].title;
+        this.nextStepTitle = getInstructions()[this.nextStepIndex].title;
     }
 }
