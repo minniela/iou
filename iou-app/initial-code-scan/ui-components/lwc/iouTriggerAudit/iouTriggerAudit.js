18c2d4fc98846e8e87609b3da808314c40f099c0
lukebuthman <lbuthman@gmail.com>
2023-09-05 10:24:48 -0500

    implement message subscriber

--- iou-app/initial-code-scan/ui-components/lwc/iouTriggerAudit/iouTriggerAudit.js
+++ iou-app/initial-code-scan/ui-components/lwc/iouTriggerAudit/iouTriggerAudit.js
@@ -1,5 +1,25 @@
-import {LightningElement} from 'lwc';
+import {LightningElement, wire} from 'lwc';
+import { subscribe, MessageContext } from 'lightning/messageService';
+import IOU_POPULATED from '@salesforce/messageChannel/IOU_Populated__c';
 
 export default class IOUTriggerAudit extends LightningElement {
 
+    @wire(MessageContext)
+    messageContext;
+
+    connectedCallback() {
+        this.subscribeToMessageChannel();
+    }
+
+    subscribeToMessageChannel() {
+        this.subscription = subscribe(
+            this.messageContext,
+            IOU_POPULATED,
+            (message) => this.handleMessage(message)
+        );
+    }
+
+    handleMessage(message) {
+        alert('got it!');
+    }
 }
