f22cfd2984b541d754689d997af747eb666ac779
lukebuthman <lbuthman@gmail.com>
2023-09-04 19:34:00 -0500

    update instructions

--- iou-app/initial-code-scan/ui-components/lwc/iouSetupInstructions/instructions.js
+++ iou-app/initial-code-scan/ui-components/lwc/iouSetupInstructions/instructions.js
@@ -2,21 +2,29 @@ const getInstructions = () => {
     return [
         {
             title: 'Download Ruleset:',
-            instruction: 'click the link below to download',
+            instruction: 'visit the link below to download',
             link: 'https://github.com/minniela/iou',
             linkText: 'IOU GitHub Repo'
         },
         {
+            title: 'Install SF CLI:',
+            instruction: 'skip this if already installed',
+            link: 'https://developer.salesforce.com/docs/atlas.en-us.sfdx_setup.meta/sfdx_setup/sfdx_setup_install_cli.htm',
+            linkText: "Salesforce CLI Download"
+        },
+        {
             title: 'Install SFCA:',
             instruction: '<div class="slds-text-font_monospace">sfdx plugins:install @salesforce/sfdx-scanner</div>',
-            link: ''
+            link: 'https://forcedotcom.github.io/sfdx-scanner/en/v3.x/getting-started/install/',
+            linkText: "SFCA Install Details"
         },
         {
             title: 'Run SFCA:',
             instruction: '<div class="slds-text-font_monospace">' +
                 'sfdx scanner:run -f json -o scans/initialCodeScan.json -t force-app --pmdconfig level-0-tech-debt-ruleset.xml' +
                 '</div>',
-            link: ''
+            link: 'https://forcedotcom.github.io/sfdx-scanner/en/v3.x/scanner-commands/run/',
+            linkText: "SFCA Command Reference"
         },
         {
             title: 'Upload JSON:',
