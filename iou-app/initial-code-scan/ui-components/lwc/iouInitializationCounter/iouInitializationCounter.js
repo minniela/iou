abc87e27d3c6f8b9b432a8135da775e7c5c7b99f
lukebuthman <lbuthman@gmail.com>
2023-09-06 13:10:06 -0500

    call service when connected or when api called

--- iou-app/initial-code-scan/ui-components/lwc/iouInitializationCounter/iouInitializationCounter.js
+++ iou-app/initial-code-scan/ui-components/lwc/iouInitializationCounter/iouInitializationCounter.js
@@ -1,4 +1,5 @@
 import {LightningElement, wire, api} from 'lwc';
+import getCountDetails from '@salesforce/apex/InitializationCounterService.getViolationCountDetails';
 
 import { publish, MessageContext } from 'lightning/messageService';
 import IOU_POPULATED from '@salesforce/messageChannel/IOU_Populated__c';
@@ -8,9 +9,24 @@ export default class IouInitializationCounter extends LightningElement {
     @wire(MessageContext)
     messageContext;
 
+    connectedCallback() {
+        this.publishCountData();
+    }
+
     @api
     publishCountData() {
         //todo: if this wire data, would it re-fire when data was populated
-        publish(this.messageContext, IOU_POPULATED);
+        this.checkForCountDetails();
+        //publish(this.messageContext, IOU_POPULATED);
+    }
+
+    checkForCountDetails() {
+        getCountDetails()
+            .then(response => {
+                console.log(JSON.stringify(response));
+            })
+            .catch(error => {
+                console.log(JSON.stringify(error));
+            })
     }
 }
