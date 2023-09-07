2ce82f320813668333920e5855648d73a481552c
lukebuthman <lbuthman@gmail.com>
2023-09-07 08:31:55 -0500

    add todos

--- iou-app/initial-code-scan/ui-components/lwc/iouStartOver/iouStartOver.js
+++ iou-app/initial-code-scan/ui-components/lwc/iouStartOver/iouStartOver.js
@@ -3,6 +3,12 @@ import startOver from "@salesforce/apex/InitialIngestor.startOver";
 
 export default class IouStartOver extends LightningElement {
 
+    //todo: prompt user to confirm the destructive action
+    //todo: add a response from the apex
+    //todo: handle response and toast any error
+    //todo: refresh the page upon deletion of records, instead of another layer of message service
+    //todo: subscribe to message service to display conditionally
+
     handleDelete() {
         startOver()
             .then(() => {
