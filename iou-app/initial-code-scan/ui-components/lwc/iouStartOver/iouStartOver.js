a07574be12129ecfda6c4d46e93d873b17d7ebc3
lukebuthman <lbuthman@gmail.com>
2023-09-07 08:10:14 -0500

    imperatively call apex, simple version

--- iou-app/initial-code-scan/ui-components/lwc/iouStartOver/iouStartOver.js
+++ iou-app/initial-code-scan/ui-components/lwc/iouStartOver/iouStartOver.js
@@ -1,5 +1,15 @@
 import {LightningElement} from 'lwc';
+import startOver from "@salesforce/apex/InitialIngestor.startOver";
 
 export default class IouStartOver extends LightningElement {
 
+    handleDelete() {
+        startOver()
+            .then(() => {
+
+            })
+            .catch(error => {
+
+            })
+    }
 }
