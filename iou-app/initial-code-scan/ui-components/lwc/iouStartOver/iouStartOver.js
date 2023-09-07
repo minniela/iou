4df886bd7fde172299bb66d7ec4357ab08139f56
lukebuthman <lbuthman@gmail.com>
2023-09-07 08:46:30 -0500

    add confirmation upon start over click

--- iou-app/initial-code-scan/ui-components/lwc/iouStartOver/iouStartOver.js
+++ iou-app/initial-code-scan/ui-components/lwc/iouStartOver/iouStartOver.js
@@ -1,13 +1,24 @@
 import {LightningElement} from 'lwc';
+import LightningConfirm from 'lightning/confirm';
 import startOver from "@salesforce/apex/InitialIngestor.startOver";
 
 export default class IouStartOver extends LightningElement {
 
-    //todo: prompt user to confirm the destructive action
     //todo: add a response from the apex
     //todo: handle response and toast any error
     //todo: refresh the page upon deletion of records, instead of another layer of message service
     //todo: subscribe to message service to display conditionally
+
+    async requireConfirmation() {
+        const confirmed = await LightningConfirm.open({
+            message: 'Do you want to proceed?',
+            label: 'Confirm Record Deletion',
+            theme: 'warning'
+        });
+        if (confirmed) {
+            this.handleDelete();
+        }
+    }
 
     handleDelete() {
         startOver()
