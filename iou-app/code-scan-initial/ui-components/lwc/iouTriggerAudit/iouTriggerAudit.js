db373ce5536c4daeddfc1417d798d18357395a9b
lukebuthman <lbuthman@gmail.com>
2024-10-15 19:26:06 -0500

    add trigger audit feature

--- iou-app/code-scan-initial/ui-components/lwc/iouTriggerAudit/iouTriggerAudit.js
+++ iou-app/code-scan-initial/ui-components/lwc/iouTriggerAudit/iouTriggerAudit.js
@@ -1,8 +1,12 @@
 import {LightningElement, wire, track} from 'lwc';
 import { subscribe, MessageContext } from 'lightning/messageService';
 import IOU_POPULATED from '@salesforce/messageChannel/IOU_Populated__c';
+import flagApex from '@salesforce/apex/TriggerAuditController.flagApexClass';
+import Toast from 'lightning/toast';
 
 export default class IOUTriggerAudit extends LightningElement {
+
+    className;
 
     @wire(MessageContext)
     messageContext;
@@ -25,5 +29,29 @@ export default class IOUTriggerAudit extends LightningElement {
 
     handleMessage(payload) {
         this.classesCreated = payload.classCount > 0;
+    }
+
+    saveClass() {
+        flagApex({className: this.className})
+            .then(wasSuccessful => {
+                if (wasSuccessful === true) {
+                    this.showToast('The Apex Class was successfully audited!', 'success');
+                }
+                else {
+                    this.showToast('The class was unable to be saved. Checks the debug logs for more info.', 'error');
+                }
+            })
+            .catch(error => {
+                this.showToast('The class was unable to be saved. See the browser\'s console output for more info.', 'error');
+                console.log(JSON.stringify(error));
+            })
+    }
+
+    showToast(message, result) {
+        Toast.show({
+            label: 'Trigger Audit Save Result',
+            message: message,
+            variant: result
+        }, this);
     }
 }
